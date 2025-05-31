a3ef3265309d8ba20bf7b4d2fe6ef3be65145220
Eduardo\Eduardo Oliveira <eduardo.olivei@outlook.com>
2025-05-31 02:58:14 -0400

    feat: adicionar configuração do express para usar body-parser e definir rotas de usuário

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import { json } from 'body-parser';
 import { connectToDatabase } from './database/prisma';
+import userRoutes from './routes/user.route';
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.use(json());
+
+//Routes
+app.use('/api/v1/user', userRoutes);
 
 const startServer = async () => {
     try {
