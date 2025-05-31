ba080ce5ffd0fe7c90e28592d09ea2028d1dc983
Eduardo\Eduardo Oliveira <eduardo.olivei@outlook.com>
2025-05-30 22:33:45 -0400

    feat: adicionar Prisma ORM com esquema inicial do banco de dados e migrações
    Atualizado o package.json para incluir as dependências do Prisma.
    
    Criada migração inicial para as tabelas User, Role e Student.
    
    Definido o esquema do banco de dados no schema.prisma com os relacionamentos.
    
    Adicionado arquivo de migração para controle de versão.

---
+++ backend/prisma/migrations/20250531023136_init/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "roleId" INTEGER,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "ra" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_cpf_key" ON "User"("cpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_ra_key" ON "Student"("ra");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE SET NULL ON UPDATE CASCADE;
