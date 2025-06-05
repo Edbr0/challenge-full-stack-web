167b7f2f4f8900e88dd17c1e02de018af5e3e0df
Eduardo\Eduardo Oliveira <eduardo.olivei@outlook.com>
2025-06-05 04:43:38 -0400

    feat: adicionar lógica de autenticação e rotas para o layout Admin

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,11 +6,51 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto-routes'
+import AdminLayout from '@/layouts/AdminLayout.vue'
+
+import Login from '@/pages/Login.vue'
+import Students from '@/pages/Students.vue'
+import { useAuthStore } from '@/store'
+
+const routes = [
+  {
+    path: '/',
+    component: Login, // Login sem layout
+  },
+  {
+    path: '/',
+    component: AdminLayout,
+    children: [
+      {
+        path: 'students',
+        component: Students,
+      },
+      // outras rotas protegidas aqui
+    ],
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/') {
+    return next()
+  }
+
+  const authStore = useAuthStore()
+
+  const token = authStore.token
+
+  if (!token) {
+    // Se não estiver logado, redireciona para a página inicial
+    return next({ path: '/' })
+  }
+
+  // Se estiver logado, permite acesso
+  next()
 })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
